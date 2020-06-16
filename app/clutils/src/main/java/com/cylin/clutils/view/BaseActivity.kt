9e813c4108c2ff186abc50cf646cf299510a74be
Charlie <linchiayu0217@gmail.com>
2020-06-16 16:05:55 +0800

    Rewrite Activity

---
+++ app/clutils/src/main/java/com/cylin/clutils/view/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.cylin.clutils.view
+
+import android.app.Activity
+
+open class BaseActivity: Activity() {
+
+    protected val TAG = this.javaClass.simpleName
+}
---
+++ app/clutils/src/main/java/com/cylin/clutils/view/BaseCompatActivity.kt
@@ -0,0 +1,8 @@
+package com.cylin.clutils.view
+
+import android.support.v7.app.AppCompatActivity
+
+open class BaseCompatActivity: AppCompatActivity() {
+
+    protected val TAG = this.javaClass.simpleName
+}
