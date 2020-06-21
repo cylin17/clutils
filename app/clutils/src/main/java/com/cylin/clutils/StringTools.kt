96915e0c9a83450151aca2ac69a9e886116c59ed
Charlie <linchiayu0217@gmail.com>
2020-06-21 23:58:36 +0800

    Add url postfix check

--- app/clutils/src/main/java/com/cylin/clutils/StringTools.kt
+++ app/clutils/src/main/java/com/cylin/clutils/StringTools.kt
@@ -2,10 +2,15 @@ package com.cylin.clutils
 
 import java.security.MessageDigest
 
-fun String.md5(): String {
+/**
+ * 將字串加密成 MD5
+ */fun String.md5(): String {
     return hashString(this, "MD5")
 }
 
+/**
+ * 將字串加密成 SHA-256
+ */
 fun String.sha256(): String {
     return hashString(this, "SHA-256")
 }
@@ -15,4 +20,11 @@ private fun hashString(input: String, algorithm: String): String {
             .getInstance(algorithm)
             .digest(input.toByteArray())
             .fold("", { str, it -> str + "%02x".format(it) })
+}
+
+/**
+ * 檢查網址結尾是否有「/」，若沒有則加入！
+ */
+fun String.urlCheck(): String {
+    return if (this.endsWith("/", false)) this else "$this/"
 }
